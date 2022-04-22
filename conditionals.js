e8870db417d7b4348e4f7638203d9bc94cd19b67
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-04-22 04:32:35 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -10,16 +10,33 @@ console.log('Now you\'re ready to go outside!');
 */
 
 // code to inform us what kind of clothing is advisble to wear by inputting the temperatuer
-const temperature =25;
+const temperature =-15 ;
 if (temperature < 0){
-  console.log('Make sure you pick out a scarf');
+  console.log('Make sure you pick out a scarf!');
 }
 else if (temperature < 15){
-  console.log('Short sleeves won\'t cut it');
+  console.log('Short sleeves won\'t cut it!');
 
 }
 else{
-  console.log('Short sleeves are fine');
+  console.log('Short sleeves are fine.');
 
 }
-console.log('Now you\'re ready to go outside');
+console.log('Now you\'re ready to go outside!');
+
+/*The code checks if an individual is eligible to vote by checking
+the individual's age and nationality and comparing it with the criteria
+*/
+const isCitizen = true;
+const age =26;
+const raining = false;
+if (isCitizen && age>18){
+  console.log('You are eligible to vote.');
+}
+//more conditionals examples to check the weather
+if(temperature < -40 || temperature >40){
+  console.log('Maybe going outside isn\'t such a great idea...');
+}
+if (!raining){
+  console.log('leave your umbrella at home!');
+}
